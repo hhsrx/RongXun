aad51e4bac76eddd4e5c6ce3e8b44cb2a00c341b
hhsrx <105556101+hhsrx@users.noreply.github.com>
2024-03-25 01:11:16 +0800

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -1,6 +1,6 @@
 from pyecharts.faker import Faker
 from pyecharts import options as opts
-from pyecharts.charts import Geo
+from pyecharts.charts import Map,Geo
 from pyecharts.globals import ChartType, SymbolType
 import csv
 
@@ -38,19 +38,20 @@ geo.render('./output/gdp_per_capita.html')
 
 
 c = (
-    Geo()
-    .add_schema(maptype="china")
+    Map()
 #    .add("geo", [list(z) for z in zip(Faker.provinces, Faker.values())])
     .add('', 
-         [("北京", 190091), ("上海", 179401), 
-          ("江苏", 144475), ("福建", 126845), 
-          ("浙江", 118830)],
-        #省市标记参数是由元组项组成的列表，图上标记的颜色和数值有关
-        )
+         [("甘肃省",44986),("黑龙江省",50883),("广西壮族自治区",52215),("贵州省",52348),("吉林省",55033),("河北省",56888),\
+          ("西藏自治区",58269),("青海省",60776),("云南省",61736),("河南省",62071),("海南省",66845),("四川省",67785),\
+              ("辽宁省",68515),("新疆维吾尔自治区",68526),("宁夏回族自治区",69925),("江西省",71009),("湖南省",73498),("山西省",73686),\
+                  ("安徽省",73687),("陕西省",82885),("山东省",85973),("重庆市",90688),("湖北省",92170),("内蒙古自治区",96496),\
+                      ("广东省",101796),("天津市",118801),("浙江省",118830),("福建省",126845),("江苏省",144475),\
+                          ("上海市",179401),("北京市",190091)],
+        "china")
     .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
     .set_global_opts(
-        visualmap_opts=opts.VisualMapOpts(min_=100000, max_=200000), 
-        title_opts=opts.TitleOpts(title="中国2022各省市人均GDP前五（单位：元）")
+        visualmap_opts=opts.VisualMapOpts(min_=44986, max_=190091, is_piecewise=True), 
+        title_opts=opts.TitleOpts(title="中国2022各省市人均GDP（单位：元）")
     )
     .render("./output/gdp_provinces.html")
 )
